5537c3c277644540f77ae1ff5d20fa6451fa3617
Sergei Egorov <null@egorbos.codes>
2023-06-12 18:31:05 +1000

    Добавлено решение задачи № 1.3

--- Lesson 1/Program.java
+++ Lesson 1/Program.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Program {
 
+    // Задача № 1
     private static void taskOne(int number) {
         int summ = 1, factorial = 1;
         for (var i = 2; i <= number; i++) {
@@ -14,6 +14,7 @@ public class Program {
         System.out.printf("Факториал числа %d равен %d\n", number, factorial);
     }
 
+    // Задача № 2
     private static boolean isNumberPrime(int number) {
         if (number <= 1) return false;
         if (number == 2) return true;
@@ -29,16 +30,27 @@ public class Program {
         return true;        
     }
 
+    // Задача № 3
+    private static double calculate(double firstNumber, double secondNumber, String action) {
+        switch (action) {
+            case "+": return firstNumber + secondNumber;
+            case "-": return firstNumber - secondNumber;
+            case "*": return firstNumber * secondNumber;
+            case "/": return firstNumber / secondNumber;
+            default: return 0;
+        }
+    }
+
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
 
-        // Задача № 1
+        //
         System.out.println("Задача № 1");
         System.out.print("Введите число: ");
         var number = scanner.nextInt();
         taskOne(number);
 
-        // Задача № 2
+        //
         System.out.println("\nЗадача № 2");
         var primeNumbers = new ArrayList<Integer>();
         for (var i = 1; i <= 1000; i++) {
@@ -47,6 +59,17 @@ public class Program {
             }
         }
         System.out.println("Простые числа в диапазоне [1; 1000]: " + primeNumbers.toString());
+
+        //
+        System.out.println("\nЗадача № 3");
+        System.out.print("Введите первое число: ");
+        var firstNumber = Double.parseDouble(scanner.next());
+        System.out.print("Введите действие: ");
+        var action = scanner.next();
+        System.out.print("Введите второе число: ");
+        var secondNumber = Double.parseDouble(scanner.next());
+        var result = calculate(firstNumber, secondNumber, action);
+        System.out.printf("%.2f %s %.2f = %.2f\n", firstNumber, action, secondNumber, result);
 
         scanner.close();
     }
